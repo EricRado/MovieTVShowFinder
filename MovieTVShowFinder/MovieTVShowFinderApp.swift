3314f7a9dff524bbbac99fcf47f827b46acb2aaf
Eric Rado <ericvictor14@hotmail.com>
2024-08-02 01:06:40 -0400

    Initial Commit

---
+++ MovieTVShowFinder/MovieTVShowFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTVShowFinderApp.swift
+//  MovieTVShowFinder
+//
+//  Created by Eric Rado on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieTVShowFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
