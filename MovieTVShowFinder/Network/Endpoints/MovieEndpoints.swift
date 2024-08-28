12b2609df99b764062ba43faee8905722a64023b
Eric Rado <ericvictor14@hotmail.com>
2024-08-28 13:57:18 -0400

    Fix url path, add image storage url path and headers to support bearer token

---
+++ MovieTVShowFinder/Network/Endpoints/MovieEndpoints.swift
@@ -0,0 +1,8 @@
+//
+//  MovieEndpoints.swift
+//  MovieTVShowFinder
+//
+//  Created by Eric Rado on 8/3/24.
+//
+
+import Foundation
---
+++ MovieTVShowFinder/Styling/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  MovieTVShowFinder
+//
+//  Created by Eric Rado on 8/26/24.
+//
+
+import Foundation
