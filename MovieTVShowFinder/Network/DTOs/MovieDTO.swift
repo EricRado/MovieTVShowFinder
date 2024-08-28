f0ce5a96eccd4a529de486db47677a6be0b51619
Eric Rado <ericvictor14@hotmail.com>
2024-08-28 14:01:27 -0400

    Add endpoints to support retrieval of movies

--- MovieTVShowFinder/Network/DTOs/MovieDTO.swift
+++ MovieTVShowFinder/Network/DTOs/MovieDTO.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct MovieResponse: Decodable {
+    let page: Int
+    let results: [MovieDTO]
+    let totalPages: Int
+    let totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+struct MovieDTO: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let releaseDate: String
+    let details: String
+    private let backdropPath: String
+    private let posterPath: String
+    let genreIds: [Int]
+    let rating: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case releaseDate = "release_date"
+        case details = "overview"
+        case backdropPath = "backdrop_path"
+        case posterPath = "poster_path"
+        case genreIds = "genre_ids"
+        case rating = "vote_average"
+    }
+    
+    var backdropImageUrlString: String {
+        Network.imageHostPath + backdropPath
+    }
+    
+    var posterImageUrlString: String {
+        Network.imageHostPath + posterPath
+    }
+}
--- MovieTVShowFinder/Network/Endpoints/MovieEndpoints.swift
+++ MovieTVShowFinder/Network/Endpoints/MovieEndpoints.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+enum MovieEndpoints: Endpoint {
+    
+    enum TimeWindow: String {
+        case day
+        case week
+    }
+    
+    enum ListType: String {
+        case nowPlaying = "now_playing"
+        case popular
+        case topRated = "top_rated"
+        case upcoming
+    }
+    
+    case list(ListType, pageCount: Int)
+    case trending(TimeWindow = .day)
+    
+    var path: String {
+        switch self {
+        case .list(let listType, _):
+            return "/movie/\(listType.rawValue)"
+        case .trending(let timeWindow):
+            return "/3/trending/movie/\(timeWindow.rawValue)"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .list, .trending:
+            return .get
+        }
+    }
+    
+    var body: [String : Any]? {
+        switch self {
+        case .list(_, let pageCount):
+            return ["page": pageCount]
+        case .trending:
+            return nil
+        }
+    }
+}
